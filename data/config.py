c060203833706a23a4b0bcefb400cc22de9d8118
Alexsey272 <alexsey272@vk.com>
2021-11-25 10:41:58 +0700

    add env.example, example.db

--- data/config.py
+++ data/config.py
@@ -6,8 +6,6 @@ env.read_env()
 TOKEN = env.str('TOKEN')
 ADMIN = env.list('ADMINS')
 BOT_OWNER = env.str('BOT_OWNER')
-PAYMENTS = env.str('PAYMENTS')
-YANDEX_API = env.str('YANDEX_API')
 
 REPORT = env.str('REPORT')
 ERROR = env.str('ERROR')
--- loader.py
+++ loader.py
@@ -1,20 +1,1 @@
-from utils.db_api.database import dbworker
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from datetime import *
-
-from data import config
-
-# время
-now = datetime.now()
-now = now.strftime("%Y-%m-%d")
-now = datetime.strptime(now, '%Y-%m-%d')
-
-if not config.TOKEN:
-    exit("No token provided")
-
-bot = Bot(token=config.TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot, storage = MemoryStorage())
-
-# инициализируем базу данных
-db = dbworker('db dumb1.db')
+from utils.db_api.database import dbworkerfrom aiogram import Bot, Dispatcher, typesfrom aiogram.contrib.fsm_storage.memory import MemoryStoragefrom datetime import *from data import config# времяnow = datetime.now()now = now.strftime("%Y-%m-%d")now = datetime.strptime(now, '%Y-%m-%d')if not config.TOKEN:    exit("No token provided")bot = Bot(token=config.TOKEN, parse_mode="HTML")dp = Dispatcher(bot, storage = MemoryStorage())# инициализируем базу данныхdb = dbworker('db_dumb1.db')
